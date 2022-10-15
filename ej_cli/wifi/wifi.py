1cb8da0dedbb8fa71e436bab014c0e3aa9eb1419
Abdelrahman <20213927+Abdelrahman0W@users.noreply.github.com>
2022-10-15 14:04:15 +0200

    _

--- ej_cli/wifi/wifi.py
+++ ej_cli/wifi/wifi.py
@@ -6,5 +6,11 @@ from .winwifi import WinWiFi
 
 
 class WiFi:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, uid: str, pwd: str) -> None:
+        self.user = User(uid, pwd).auth_ejustian
+        if self.user["auth"]:
+            self.user_name = self.user["name"]
+            self.known_list = self.user["wifi_list"]
+            self.ssid_list = list(self.known_list.keys())
+        else:
+            self.user_name = self.known_list = self.ssid_list = None
