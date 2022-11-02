78320fd64feab154c05dc5b2f62bc09cf335a5a9
Abdelrahman <20213927+Abdelrahman0W@users.noreply.github.com>
2022-11-02 08:11:46 +0200

    added: linux wifi
    connect to nearest wifi support for linux

--- ej_cli/wifi/linuxwifi.py
+++ ej_cli/wifi/linuxwifi.py
@@ -1,3 +1,71 @@
+import subprocess
+
+
 class LinuxWiFi:
     def __init__(self) -> None:
         pass
+
+    @property
+    def is_active(self) -> bool:
+        process = subprocess.run(
+            ["nmcli", "-t", "-f", "ACTIVE,SSID", "dev", "wifi"],
+            stdout=subprocess.PIPE
+        )
+
+        if process.returncode != 0:
+            return False
+
+        try:
+            wifi_list = [
+                wifi.split(":")
+                for wifi in process.stdout.decode("utf-8").strip().split("\n")
+            ]
+            return any("yes" in wifi for wifi in wifi_list)
+        except Exception:
+            return False
+
+    @property
+    def scan(self) -> list:
+        process = subprocess.run(
+            ["nmcli", "-t", "-f", "SSID,SIGNAL", "dev", "wifi"],
+            stdout=subprocess.PIPE
+        )
+
+        if process.returncode != 0:
+            return []
+
+        try:
+            return [
+                wifi.split(":") for wifi in process.stdout.decode("utf-8").strip().split("\n")
+            ]
+        except Exception:
+            return []
+
+    def is_connected_to(self, ssid: str) -> bool:
+        process = subprocess.run(
+            ["nmcli", "-t", "-f", "ACTIVE,SSID", "dev", "wifi"],
+            stdout=subprocess.PIPE
+        )
+
+        if process.returncode != 0:
+            return False
+
+        try:
+            wifi_list = [
+                wifi.split(":")
+                for wifi in process.stdout.decode("utf-8").strip().split("\n")
+            ]
+            return ["yes", ssid] in wifi_list
+        except Exception:
+            return False
+
+    def connect_to(self, ssid: str, password: str):
+        subprocess.call(
+            [
+                'nmcli', 'd', 'wifi',
+                'connect', ssid,
+                'password', password
+            ]
+        )
+
+        return "CONNECTED SUCCESSFULLY!" if self.is_connected_to(ssid) else "AN ERROR OCCURED!"
