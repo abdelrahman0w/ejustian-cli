4112614270f869e6ab691e2e0913fcd63c603b1b
Abdelrahman Nawar <abdonawar80@gmail.com>
2022-11-10 19:49:00 +0200

    #20-classroom-show-classes

--- ej_cli/classroom/classroom.py
+++ ej_cli/classroom/classroom.py
@@ -3,7 +3,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-
+import os
+from typing import Any, List
 
 # Implement Google Classroom API
 # https://developers.google.com/classroom/quickstart/python
@@ -17,6 +18,7 @@ from googleapiclient.errors import HttpError
 # TODO: Show Class Materials
 # TODO: Show Class Posts
 
+SCOPES = ['https://www.googleapis.com/auth/classroom.courses.readonly']
 
 class Classroom:
     def __init__(self) -> None:
@@ -34,11 +36,17 @@ class Classroom:
         """
         pass
 
-    def show_classes(self):
+    def show_classes(self) -> List[str]:
         """
         Show current student classes.
         """
-        pass
+        courses : List[dict] = self.__get_courses()
+        course_names : List[str] = []
+        if courses:
+            for course in courses:
+                course_names.append(course['name'])
+
+        return course_names
 
     def submit_assignment(self):
         """
@@ -81,3 +89,42 @@ class Classroom:
         Show class work.
         """
         pass
+
+    """
+    Private methods
+    """
+
+    def __autenticate(self) -> Any: # TODO : create a generic type for the credentials
+        """
+        Authenticates using OAuth2
+        """
+        creds = None
+
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        return creds
+
+    def __get_courses(self) -> List[dict]:
+        """
+        Get all courses that the user is enrolled in
+        """
+        creds = self.__autenticate()
+        try:
+            service = build('classroom', 'v1', credentials=creds)
+            results = service.courses().list().execute()
+            courses = results.get('courses', [])
+        except HttpError as error:
+            print('An error occurred: %s' % error)
+
+        return courses
